cdac90ba03a643a5794f98b8f2e8d69a96792540
Shuhua Shrestha <71085603+shuhua833@users.noreply.github.com>
2022-08-29 18:02:19 +0545

    Create banker.c

---
+++ banker.c
@@ -0,0 +1,84 @@
+// Banker's Algorithm
+#include <stdio.h>
+int main()
+{
+	// P0, P1, P2, P3, P4 are the Process names here
+
+	int n, m, i, j, k;
+	n = 5; // Number of processes
+	m = 4; // Number of resources
+	int alloc[5][4] = { { 0,0,1,2 }, // P0 // Allocation Matrix
+						{ 1,0,0,0 }, // P1
+						{ 1,3,5,4 }, // P2
+						{ 0,6,3,2 }, // P3
+						{ 0,0,1,4 } }; // P4
+
+	int max[5][4] = { { 0,0,1,2 }, // P0 // MAX Matrix
+					{ 1,7,5,0}, // P1
+					{ 2,3,5,6 }, // P2
+					{ 0,6,5,2 }, // P3
+					{ 0,6,5,6 } }; // P4
+
+	int avail[4] = { 1,5,2,0 }; // Available Resources
+
+	int f[n], ans[n], ind = 0;
+	for (k = 0; k < n; k++) {
+		f[k] = 0;
+	}
+	int need[n][m];
+	printf("Need matrix:\n");
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++){
+			need[i][j] = max[i][j] - alloc[i][j];
+			printf("%d\t",need[i][j]);
+		}
+		printf("\n");
+	}
+	int y = 0;
+	for (k = 0; k < 5; k++) {
+		for (i = 0; i < n; i++) {
+			if (f[i] == 0) {      //to redo the processes which are not in safe state
+
+				int flag = 0;
+				for (j = 0; j < m; j++) {
+					if (need[i][j] > avail[j]){
+						flag = 1;
+						break;             //process not in safe state
+					}
+				}
+
+				if (flag == 0) {
+					ans[ind++] = i;
+					for (y = 0; y < m; y++)
+						avail[y] += alloc[i][y];
+					f[i] = 1;                   //to prevent the iteration of processes that are already in safe state
+				}
+			}
+		}
+	}
+
+	int flag = 1;
+	
+	for(int i=0;i<n;i++)
+	{
+	if(f[i]==0)                            //initially all f[i] were 0, processes that entered safe state were updated with one
+	{
+		flag=0;
+		printf("The following system is not safe");
+		break;
+	}
+	}
+	
+	if(flag==1)
+	{
+	printf("Following is the SAFE Sequence\n");
+	for (i = 0; i <=n - 1; i++)
+		printf(" P%d ->", ans[i]);
+
+	}
+	
+
+	return (0);
+
+	
+}
